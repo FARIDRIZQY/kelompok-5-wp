d5ca6b7b57b801ee4792236bbeeca8da303b7d2e
FARIDRIZQY <faridivanrizqy501@gmail.com>
2023-12-21 21:37:21 +0700

    first commit

---
+++ application/logs/log-2023-07-15.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-07-15 21:05:16 --> 404 Page Not Found: Public/images
---
+++ application/logs/log-2023-12-03.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-12-03 22:00:46 --> 404 Page Not Found: Public/images
+ERROR - 2023-12-03 22:01:18 --> Query error: Table 'inventorysalesci.admin' doesn't exist - Invalid query: SELECT password FROM admin WHERE email = 'admin@gmail.com'
+ERROR - 2023-12-03 22:01:19 --> Query error: Table 'inventorysalesci.admin' doesn't exist - Invalid query: SELECT password FROM admin WHERE email = 'admin@gmail.com'
+ERROR - 2023-12-03 22:01:21 --> Query error: Table 'inventorysalesci.admin' doesn't exist - Invalid query: SELECT password FROM admin WHERE email = 'admin@gmail.com'
+ERROR - 2023-12-03 22:01:21 --> Query error: Table 'inventorysalesci.admin' doesn't exist - Invalid query: SELECT password FROM admin WHERE email = 'admin@gmail.com'
+ERROR - 2023-12-03 22:01:22 --> Query error: Table 'inventorysalesci.admin' doesn't exist - Invalid query: SELECT password FROM admin WHERE email = 'admin@gmail.com'
